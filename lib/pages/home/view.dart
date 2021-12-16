0f158ce21f0dc9c293cd8781ac87fceb8cf898a6
Serkan Kavak <serkankavakis@gmail.com>
2021-12-16 21:10:03 +0800

    to be continued on the weekend

--- lib/pages/home/view.dart
+++ lib/pages/home/view.dart
@@ -2,7 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:world_map/world_map.dart';
 
-import '../../i18n/language_controller.dart';
+// import '../../i18n/language_controller.dart';
 import 'controller.dart';
 import '../../components/drawer.dart';
 
@@ -11,21 +11,22 @@ class HomePage extends GetView<HomeController> {
 
   @override
   Widget build(BuildContext context) {
-    LanguageController languageController = Get.find();
+    // LanguageController languageController = Get.find();
     final height = MediaQuery.of(context).size.height;
     final width = MediaQuery.of(context).size.width;
     return Scaffold(
       appBar: AppBar(
         title: Text('home.title'.tr),
-        actions: [
-          TextButton(
-            onPressed: () => controller.toggleLanguage(),
-            child: Text(
-              languageController.lang.toUpperCase(),
-              style: const TextStyle(fontSize: 25, color: Colors.white),
-            ),
-          )
-        ],
+        // TODO: Add i18n if it needs in the future
+        // actions: [
+        //   TextButton(
+        //     onPressed: () => controller.toggleLanguage(),
+        //     child: Text(
+        //       languageController.lang.toUpperCase(),
+        //       style: const TextStyle(fontSize: 25, color: Colors.white),
+        //     ),
+        //   )
+        // ],
       ),
       drawer: const MainDrawer(),
       body: Center(
@@ -35,6 +36,60 @@ class HomePage extends GetView<HomeController> {
           child: Column(
             crossAxisAlignment: CrossAxisAlignment.start,
             children: [
+              ElevatedButton(
+                  onPressed: () {
+                    Get.bottomSheet(
+                      Card(
+                        color: Colors.amber,
+                        child: Column(
+                          mainAxisSize: MainAxisSize.min,
+                          children: <Widget>[
+                            Row(
+                              children: const [
+                                Icon(Icons.flag),
+                                Text('Name of the country'),
+                              ],
+                            ),
+                            Row(
+                              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                              mainAxisSize: MainAxisSize.min,
+                              children: [
+                                Expanded(
+                                  child: CheckboxListTile(
+                                    title: const Text('Been'),
+                                    value: false,
+                                    onChanged: (bool? val) {},
+                                    contentPadding: const EdgeInsets.symmetric(
+                                        horizontal: 4),
+                                  ),
+                                ),
+                                Expanded(
+                                  child: CheckboxListTile(
+                                    title: const Text('Want'),
+                                    value: false,
+                                    onChanged: (bool? val) {},
+                                    contentPadding: const EdgeInsets.symmetric(
+                                        horizontal: 4),
+                                  ),
+                                ),
+                                Expanded(
+                                  child: CheckboxListTile(
+                                    title: const Text('Fav'),
+                                    value: false,
+                                    onChanged: (bool? val) {},
+                                    contentPadding: const EdgeInsets.symmetric(
+                                        horizontal: 4),
+                                  ),
+                                )
+                              ],
+                            ),
+                          ],
+                        ),
+                      ),
+                      barrierColor: Colors.transparent,
+                    );
+                  },
+                  child: const Text('selam')),
               // Map
               Expanded(
                 child: InteractiveViewer(
--- packages/touchable_canvas/lib/src/touchy_canvas.dart
+++ packages/touchable_canvas/lib/src/touchy_canvas.dart
@@ -1,5 +1,3 @@
-import 'dart:ui';
-import 'package:flutter/gestures.dart';
 import 'package:flutter/material.dart';
 
 import 'canvas_touch_detector.dart';
